744f43b1a22994f87199e7d470fe1a845a389489
louannmonjarret <louann.monjarret@gmail.com>
2026-03-24 08:49:12 +0100

    Add files via upload

---
+++ function_def.h
@@ -0,0 +1,17 @@
+#ifndef FUNCTION_DEF_H
+#define FUNCTION_DEF_H
+
+void hello_world();
+
+int compare();
+
+void loop_1_100();
+
+void while_1_100();
+
+void assign();
+
+int sum();
+
+#endif
+
---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "function_def.h"
+
+/*
+int main()
+{
+	hello_world();
+	return 0;
+}
+
+
+int main()
+{
+	compare(20,20);
+	return 0;
+}
+
+
+int main()
+{
+	loop_1_100();
+	return 0;
+
+}
+
+int main()
+{
+	while_1_100();
+	return 0;
+}
+
+
+
+int main()
+{
+	int x = 10;
+	assign(x);
+	return 0;
+}
+*/
+
+int main()
+{
+	int somme = sum(1,2);
+	printf("%d \n",somme);
+	return 0;
+}
